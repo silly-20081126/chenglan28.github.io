b203b2764cb928d67432f13fa792cc34ec4a8a3e
air_studios <76613821+chenglan28@users.noreply.github.com>
2021-03-07 14:47:29 +0800

    Add files via upload

--- avs/demo/system/js/command.js
+++ avs/demo/system/js/command.js
@@ -1,4 +1,4 @@
-function command(value1,value2,value3){
+function command(value1,value2,value3,value4){
     if(value1 == "demo"){
         if(value2 == "width"){
             document.getElementById("demo").style.width = value3;
@@ -10,4 +10,9 @@ function command(value1,value2,value3){
             document.getElementById("demo").style.backgroundImage = value3;
         }
     }
+    if(value1 == "icon"){
+        demo = document.getElementById("demo").innerHTML;
+        document.getElementById("demo").innerHTML = "<a onclick='" + value4 + "'><img class='icon' title='" + value2 + "' src='" + value3 + "'></a>";
+    }
+    //if(value1 == )
 }
--- avs/demo/system/js/system.js
+++ avs/demo/system/js/system.js
@@ -1,3 +1,4 @@
     command("demo","width","1920px");
     command("demo","height","1080px");
     command("demo","backgroundImage","url(system/img/backgroundImage.png)");
+    command("icon","帮助","system/img/icon.png",'command("open","help")');
