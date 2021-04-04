817eba7293a66f7d8e1f284aac31dce40d87db9f
air_studios <76613821+chenglan28@users.noreply.github.com>
2021-04-04 14:47:39 +0800

    Delete avs directory

--- avs/demo/system/js/system.js
+++
@@ -1,4 +0,0 @@
-    command("demo","width","1920px");
-    command("demo","height","1080px");
-    command("demo","backgroundImage","url(system/img/backgroundImage.png)");
-    command("icon","帮助","system/img/icon.png",'command("open","help")');
